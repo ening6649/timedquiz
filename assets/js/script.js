08510fc92579302f3e824a1f55e0176c08919011
ening <ening6649@yahoo.com>
2021-10-23 14:01:25 -0400

    fixed timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,62 @@
 
 
 
-var startQuiz = function() {
-   questionAsk();
-   choiceList();
-}
+// var startQuiz = function() {
+//    questionAsk();
+//    choiceList();
+// }
 
-var questionAsk = function() {
-    var questionAskEl = document.createElement ("h1");
-    questionAskEl.textContent = "blah blah blah";
-    questionAskEl.className = "questions";
-    questionAskEl.appendChild(quiz);
-}
+// var questionAsk = function() {
+//     var questionAskEl = document.createElement ("h1");
+//     questionAskEl.textContent = "blah blah blah";
+//     questionAskEl.className = "questions";
+//     questionAskEl.appendChild(quiz);
+// }
 
-var choiceList = function () {
-    var choiceListEl = document.createElement ("li");
-        choiceListEl.textContent = "";
-        questionlistEl.className = "choices"
-        startButtonEl.appendChild(questionlistEl)
-}
+// var choiceList = function () {
+//     var choiceListEl = document.createElement ("li");
+//         choiceListEl.textContent = "";
+//         questionlistEl.className = "choices"
+//         startButtonEl.appendChild(questionlistEl)
+// }
+
+// questionAsk.className = "questions";
+// choiceList.className = "choices"
+
+
+
+// var choiceOneEl = document.querySelector("#button1");
+// var choiceTwoEl = document.querySelector("#button2");
+// var choiceThreeEl = document.querySelector("#button3");
+// var choiceFourEl =document.querySelector("#button4");
+
 
 
 var startButtonEl= document.querySelector("#start");
-
-
-
-
-var choiceOneEl = document.querySelector("#button1");
-var choiceTwoEl = document.querySelector("#button2");
-var choiceThreeEl = document.querySelector("#button3");
-var choiceFourEl =document.querySelector("#button4");
-
-
-
-var counter = 60;
 var counterEl= document.querySelector("#count");
-function setCountertext() {
-    counterEl.textContent = counter; 
-}
-var countdown = function () {
-    counter--;
-    setCountertext();
-    if (counter ===0) {
-        clearInterval(startCountdown);
-        // scorePage();
+
+var counter = 3;
+var timerRun = false;
+var startgame = function () {
+    if (timerRun ===false) {
+        timerRun = true;
+        counter = 5;
+    var stopCount = setInterval(() => {
+     counterEl.textContent= counter; 
+     counter--;
+        console.log(counter)
+        if (counter < 0) {
+            clearInterval(stopCount);
+            timerRun=false;
+            // scorePage();
+        };
+    
+    }, 1000);
     }
-
-}
-
-var startCountdown = setInterval(countdown, 1000);
-counterEl.append(counter)
+} 
 
 
+startButtonEl.addEventListener("click", startgame);
 
 
 
@@ -59,5 +64,4 @@ counterEl.append(counter)
 // var timedstart = setTimeout (blah,1000); 
 
 
-startButtonEl.addEventListener("click", startQuiz);
-startButtonEl.addEventListener("click", countdown);
+// startButtonEl.addEventListener("click", startQuiz);
