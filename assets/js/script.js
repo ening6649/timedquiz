6ac8e30873465c7a01cfa85859541f1ec3fe108a
ening <ening6649@yahoo.com>
2021-10-25 19:29:42 -0400

    added main page js

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,6 +31,26 @@
 // var choiceFourEl =document.querySelector("#button4");
 
 
+// start page 
+var body = document.body;
+var MainEl=document.createElement("main");
+var divEl = document.createElement("div");
+var H2El = document.createElement ("h2");
+var pEl= document.createElement("p");
+var buttonEl = document.createElement ("button");
+divEl.className = "startpage";
+H2El.textContent = "Timed Quiz";
+pEl.textContent = "Start your quiz here";
+buttonEl.textContent ="click to start";
+buttonEl.id ="start";
+body.appendChild(MainEl);
+MainEl.appendChild(divEl);
+divEl.appendChild(H2El);
+divEl.appendChild(pEl);
+divEl.appendChild(buttonEl);
+
+
+
 
 var startButtonEl= document.querySelector("#start");
 var counterEl= document.querySelector("#count");
@@ -60,6 +80,8 @@ startButtonEl.addEventListener("click", startgame);
 
 
 
+
+
 // var timedstart = setTimeout (blah,1000); 
 
 
@@ -68,53 +90,38 @@ startButtonEl.addEventListener("click", startgame);
 // then checking which child element triggered the event. 
 // A plain JavaScript solution would look something like this:
 
-document.querySelector("#wrapper").addEventListener("click", function(event) {
- if (event.target.matches(".task")) {
-   console.log("dynamic task was clicked");
- }
-});
+// document.querySelector("#wrapper").addEventListener("click", function(event) {
+//  if (event.target.matches(".task")) {
+//    console.log("dynamic task was clicked");
+//  }
+// });
 
 
-var firstNameInput = document.querySelector("#first-name");
-var lastNameInput = document.querySelector("#last-name");
-var emailInput = document.querySelector("#email");
-var passwordInput = document.querySelector("#password");
-var signUpButton = document.querySelector("#sign-up");
 
-signUpButton.addEventListener("click", function(event) {
-  event.preventDefault();
-  
-  var user = {
-    firstName: firstNameInput.value.trim(),
-    lastName: lastNameInput.value.trim(),
-    email: emailInput.value.trim(),
-    password: passwordInput.value.trim()
-  };
-
-  // set new submission to local storage 
-  localStorage.setItem("key",JSON.stringify(user));
-  // retrives the string
-  let userString= localStorage.getItem("key");
-  // inside parse must be a string
-  let userObject = JSON.parse(userString);
-});
+//   // set new submission to local storage 
+//   localStorage.setItem("key",JSON.stringify(user));
+//   // retrives the string
+//   let userString= localStorage.getItem("key");
+//   // inside parse must be a string
+//   let userObject = JSON.parse(userString);
+// });
 
 
-infoEl.appendChild(nameEl);
-body.appendChild(favoriteEl);
+// infoEl.appendChild(nameEl);
+// body.appendChild(favoriteEl);
+// // favoriteEl.appendChild(listEl);
+// // Append ordered list 
 // favoriteEl.appendChild(listEl);
-// Append ordered list 
-favoriteEl.appendChild(listEl);
-listEl.appendChild(li1);
+// listEl.appendChild(li1);
 
 
-h1El.setAttribute("style", "margin:auto; width:50%; text-align:center;");
-infoEl.setAttribute("style", "margin:auto; width:50%; text-align:center;");
-imgEl.setAttribute("src", "http://placekitten.com/200/300");
-nameEl.setAttribute("style", "font-size:25px; text-align:center;");
-kittenEl.setAttribute("style", "font-size:25px; text-align:center;");
-favoriteEl.setAttribute("style", "font-size:20px;");
+// h1El.setAttribute("style", "margin:auto; width:50%; text-align:center;");
+// infoEl.setAttribute("style", "margin:auto; width:50%; text-align:center;");
+// imgEl.setAttribute("src", "http://placekitten.com/200/300");
+// nameEl.setAttribute("style", "font-size:25px; text-align:center;");
+// kittenEl.setAttribute("style", "font-size:25px; text-align:center;");
+// favoriteEl.setAttribute("style", "font-size:20px;");
 
-// TODO: Add ordered list items containing four favorite foods
-listEl.setAttribute("style","background-color:#333;padding:20px;");
-li1.setAttribute("style","color:white;padding:5px;margin-left:35;background-color:#f00;")
+// // TODO: Add ordered list items containing four favorite foods
+// listEl.setAttribute("style","background-color:#333;padding:20px;");
+// li1.setAttribute("style","color:white;padding:5px;margin-left:35;background-color:#f00;")
